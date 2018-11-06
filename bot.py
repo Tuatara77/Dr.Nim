3e73a4c4183ed41e1aaefc0d03e634a49a3df5b9
Tuatara77 <44623766+Tuatara77@users.noreply.github.com>
2018-11-06 18:35:48 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,15 +99,16 @@ async def ping(ctx):
     await msg.edit(content=f"Ping: ``{ping:,.2f}ms``")
 
 @bot.command()
-async def purge(ctx, ammount: int=None)
-if ammount is None:
-    await ctx.channel.purge(limit=2)
-    await ctx.send(f"{amount} messages deleted successfully", delete_after = 5)
-elif amount > 100:
-    await ctx.send(f"the limit of messages i can delete at one time is 100.", delete_after = 5)
-else:
-    clear = amount +1
-    await ctx.channel.purge(limit=clear)
-    await ctx.send(f"{amount} messages deleted successfully", delete_after = 5)
+async def purge(ctx, ammount: int=None):
+    if ammount is None:
+        await ctx.channel.purge(limit=2)
+        await ctx.send(f"{amount} messages deleted successfully", delete_after = 5)
+    elif amount > 100:
+        await ctx.send(f"the limit of messages i can delete at one time is 100.", delete_after = 5)
+    else:
+        clear = amount +1
+        await ctx.channel.purge(limit=clear)
+     await ctx.send(f"{amount} messages deleted successfully", delete_after = 5)
+
 
 bot.run(os.environ['TOKEN'])
