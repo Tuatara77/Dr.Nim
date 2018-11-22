1a7d51473207c37c3e8ea59d6a4e8c81e7348378
Tuatara77 <44623766+Tuatara77@users.noreply.github.com>
2018-11-22 18:37:57 +0000

    Update nimbot.py

--- nimbot.py
+++ nimbot.py
@@ -12,7 +12,7 @@ class Mod:
         role3 = discord.utils.get(guild.roles, id=508636962903818251)
         role4 = discord.utils.get(guild.roles, id=510818827110711296)
         role5 = discord.utils.get(guild.roles, id=466066309088804866)
-        if role1 in ctx.author.roles or role2 in ctx.author.roles or role3 in ctx.author.roles or role4 in ctx.author.roles:
+        if role1 in ctx.author.roles or role2 in ctx.author.roles or role3 in ctx.author.roles or role4 in ctx.author.roles or role5 in ctx.author.roles:
             return True
         else:
             return False
