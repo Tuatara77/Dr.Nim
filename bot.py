b679b5b4985a023be24916611aec8d46e1aa1870
Tuatara77 <44623766+Tuatara77@users.noreply.github.com>
2018-12-02 12:25:14 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ setattr(bot, "logger", logger)
 @bot.event
 async def on_ready():
     print("On standby")
-    await bot.change_presence(activity=discord.Game(name="Role Hex Code: #26f43a"))
+    await bot.change_presence(activity=discord.Game(name=" Prefix [;] | Role Hex Code: #26f43a"))
 
 @bot.command()
 async def userinfo(ctx, member: discord.Member=None):
